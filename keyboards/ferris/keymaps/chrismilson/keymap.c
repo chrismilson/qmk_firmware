f162d64b90af28376e7ffa1b2636a9e66d8f2980
Chris Milson-Tokunaga <13655076+chrismilson@users.noreply.github.com>
2022-08-16 23:54:30 +1200

    Update wasd layer

--- keyboards/ferris/keymaps/chrismilson/keymap.c
+++ keyboards/ferris/keymaps/chrismilson/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 enum layers {
     // The base layer for most english typing
     _QWERTY,
@@ -42,8 +41,6 @@ enum layers {
 #define CTL_Z LCTL_T(KC_Z)
 #define ALT_X LALT_T(KC_X)
 #define ALT_DOT LALT_T(KC_DOT)
-
-#define KC_CLN 
 
 // The keys in the middle of the board stay in the same orientation, but the
 // inside and outside columns are mirrored.
@@ -131,16 +128,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_WASD] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,      KC_H,    KC_J,    LALT_K,  KC_L,    KC_SCLN,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          MO(_WASD_AUX),    KC_SPC,    TO_SPEC, KC_LSFT
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      _______, _______, _______, _______, _______,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,      _______, _______, _______, _______, _______,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,      _______, _______, _______, _______, _______,
+                          MO(_WASD_AUX),    KC_SPC,    _______, _______,
     ),
 
     [_WASD_AUX] = LAYOUT(
-        KC_ESC,  KC_7,    KC_8,    KC_9,    KC_WH_U,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LSFT, KC_4,    KC_5,    KC_6,    KC_WH_D,   KC_H,    KC_J,    LALT_K,  KC_L,    KC_SCLN,
-        KC_LCTL, KC_1,    KC_2,    KC_3,    KC_V,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   _______, KC_SPC,    TO_SPEC, KC_LSFT
+        _______, KC_7,    KC_8,    KC_9,    KC_WH_U,   _______, _______, _______, _______, _______,
+        _______, KC_4,    KC_5,    KC_6,    KC_WH_D,   _______, _______, _______, _______, _______,
+        _______, KC_1,    KC_2,    KC_3,    _______,   _______, _______, _______, _______, _______,
+                                   _______, _______,   _______, _______,
     )
 };
