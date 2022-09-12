b5176319d2d081d82637e5d1c9a167e88390c4c5
Chris Milson-Tokunaga <13655076+chrismilson@users.noreply.github.com>
2022-09-12 18:11:07 +1200

    Use momentary layer switching
    Instead of having to do an entire 3 key presses to fire keys on
    the special layer (one press to open the layer, one to fire the key
    and one to return to the base layer), momentary layer switching means
    that the key can be fired in a single chord.
    
    This also makes it much easier to determine which layer you are on,
    as if you are not holding any keys down, then you are on the base
    layer. I developed a habit of pressing the "return to base layer"
    key pattern quite a lot and I believe it was not good for my hands to
    be typing without any need for it.

--- keyboards/ferris/keymaps/chrismilson/keymap.c
+++ keyboards/ferris/keymaps/chrismilson/keymap.c
@@ -3,18 +3,16 @@
 enum layers {
     // The base layer for most english typing
     _QWERTY,
-    // Keys that should be available on all higher layers
-    _COMMON,
     // Special characters
     _SPECIAL,
-    // Arrow key layer WIP
-    _ARROWS,
     // Numpad on right hand
     // Media keys on the left hand
     _NUMBERS,
     // Keys often required while using numbers on the right hand - plus, minus, etc.
     // Function keys on the left hand
     _NUMBERS_AUX,
+    // Arrow key layer WIP
+    _ARROWS,
 
     // EPHEMERAL LAYERS
     // The following layers are not in the same spirit as the rest of the board,
@@ -30,15 +28,11 @@ enum layers {
     _WASD_AUX,
 };
 
-#define TO_BASE TO(_QWERTY)
-#define TO_SPEC TO(_SPECIAL)
-#define TO_NUMS TO(_NUMBERS)
-#define TG_ARRW TG(_ARROWS)
 #define EP_HOME TO(_EPHEMERAL_HOME)
 #define OS_NUMX OSL(_NUMBERS_AUX)
 
-#define CTL_SLS LCTL_T(KC_SLSH)
-#define CTL_Z LCTL_T(KC_Z)
+#define GUI_SLS LGUI_T(KC_SLSH)
+#define GUI_Z LGUI_T(KC_Z)
 #define ALT_X LALT_T(KC_X)
 #define ALT_DOT LALT_T(KC_DOT)
 
@@ -67,28 +61,25 @@ enum layers {
     KC_ESC,  L02, L03, L04, L05, R01, R02, R03, R04, KC_BSPC, \
     KC_TAB,  L07, L08, L09, L10, R06, R07, R08, R09, KC_ENT, \
     KC_BTN1, L12, L13, L14, L15, R11, R12, R13, R14, R15, \
-               KC_LCTL, TO_BASE, R16, KC_LSFT \
+               KC_LCTL, _______, R16, KC_LSFT \
 )
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _SPECIAL, _NUMBERS, _ARROWS);
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT(
+    [_QWERTY] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        CTL_Z,   ALT_X,   KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, ALT_DOT, CTL_SLS,
-                                   KC_LGUI, KC_SPC,    TO_SPEC, KC_LSFT
+        GUI_Z,   ALT_X,   KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, ALT_DOT, GUI_SLS,
+              LCTL_T(KC_ENT), LT(_SPECIAL, KC_SPC),    LT(_NUMBERS, KC_BSPC), KC_LSFT
     ),
 
-	[_SPECIAL] = LAYOUT_SUPER(
+    [_SPECIAL] = LAYOUT_SUPER(
         _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_TILD, _______,
         _______, KC_EXLM, KC_EQL,  KC_MINS, KC_QUOT,   KC_GRV,  KC_BSLS, KC_LCBR, KC_RCBR, _______,
-        _______, _______, KC_PLUS, KC_UNDS, KC_DQUO,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, TG_ARRW,
-                                   _______, _______,   TO_NUMS, _______
-    ),
-
-    [_ARROWS] = LAYOUT_SUPER(
-        _______, KC_WH_D, KC_MS_U, KC_WH_U, RESET,     _______, _______, _______, _______, _______,
-        _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, KC_BTN2, _______, _______, EP_HOME,   _______, _______, _______, _______, _______,
+        _______, KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,
                                    _______, _______,   _______, _______
     ),
 
@@ -96,13 +87,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_MSTP, KC_MPRV, KC_MNXT, _______,   KC_UNDS, KC_7,    KC_8,    KC_9,    KC_BSPC,
         _______, KC_VOLD, KC_VOLU, KC_MPLY, _______,   KC_SPC,  KC_4,    KC_5,    KC_6,    KC_ENT,
         _______, KC_BRID, KC_BRIU, KC_MUTE, _______,   KC_0,    KC_1,    KC_2,    KC_3,    OS_NUMX,
-                                   _______, _______,   TO_SPEC, _______
+                                   _______, _______,   _______, _______
     ),
 
     [_NUMBERS_AUX] = LAYOUT_SUPER(
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,    _______, _______, _______, _______, _______,
         _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,    _______, KC_PLUS, KC_ASTR, KC_DOT,  _______,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,    KC_COLN, KC_MINS, KC_SLSH, KC_COMM, KC_NO,
+                                   _______, _______,   _______, _______
+    ),
+
+    [_ARROWS] = LAYOUT_SUPER(
+        _______, KC_WH_D, KC_MS_U, KC_WH_U, RESET,     _______, _______, _______, _______, _______,
+        _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, KC_BTN2, _______, _______, EP_HOME,   _______, _______, _______, _______, _______,
                                    _______, _______,   _______, _______
     ),
 
@@ -131,13 +129,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      _______, _______, _______, _______, _______,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,      _______, _______, _______, _______, _______,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,      _______, _______, _______, _______, _______,
-                          MO(_WASD_AUX),    KC_SPC,    _______, _______
+                          MO(_WASD_AUX),    KC_SPC,    TO(_QWERTY), _______
     ),
 
     [_WASD_AUX] = LAYOUT(
         _______, KC_7,    KC_8,    KC_9,    KC_WH_U,   _______, _______, _______, _______, _______,
         _______, KC_4,    KC_5,    KC_6,    KC_WH_D,   _______, _______, _______, _______, _______,
         _______, KC_1,    KC_2,    KC_3,    _______,   _______, _______, _______, _______, _______,
-                                   _______, _______,   _______, _______
+                                   _______, _______,   TO(_QWERTY), _______
     )
 };
