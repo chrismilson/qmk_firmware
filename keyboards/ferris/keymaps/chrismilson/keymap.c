342c6be2cb2775d753240b0bf96a6711ef22fb69
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2024-11-04 21:37:52 -0500

    Update keymap

--- keyboards/ferris/keymaps/chrismilson/keymap.c
+++ keyboards/ferris/keymaps/chrismilson/keymap.c
@@ -3,6 +3,8 @@
 enum layers {
     // The base layer for most english typing
     _QWERTY,
+    // Optional base layer for more better typing happy time
+    _COLEMAK,
     // Special characters
     _SPECIAL,
     // Numpad on right hand
@@ -63,7 +65,7 @@ enum layers {
 ) LAYOUT( \
     KC_ESC,  L02, L03, L04, L05, R01, R02, R03, R04, KC_BSPC, \
     KC_TAB,  L07, L08, L09, L10, R06, R07, R08, R09, KC_ENT, \
-    KC_BTN1, L12, L13, L14, L15, R11, R12, R13, R14, R15, \
+    L11    , L12, L13, L14, L15, R11, R12, R13, R14, R15, \
                KC_LCTL, _______, R16, KC_LSFT \
 )
 
@@ -71,6 +73,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _SPECIAL, _NUMBERS, _ARROWS);
 }
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
@@ -79,43 +82,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               LCTL_T(KC_ENT), LT(_SPECIAL, KC_SPC),    LT(_NUMBERS, KC_BSPC), KC_LSFT
     ),
 
+   // The layer is here because I believe, but there is no way to toggle it, because I don't believe.
+   // If we were to toggle it, it should probably be done with DF(_COLEMAK) (or _QWERTY should be removed...)
+   [_COLEMAK] = LAYOUT(
+       KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+       KC_A,    KC_R,    KC_S,    KC_T,    KC_G,      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+       GUI_Z,   ALT_X,   KC_C,    KC_D,    KC_V,      KC_K,    KC_H,    KC_COMM, ALT_DOT, GUI_SLS,
+              LCTL_T(KC_ENT), LT(_SPECIAL, KC_SPC),    LT(_NUMBERS, KC_BSPC), KC_LSFT
+   ),
+
     [_SPECIAL] = LAYOUT_SUPER(
         _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_TILD, _______,
         _______, KC_EXLM, KC_EQL,  KC_MINS, KC_QUOT,   KC_GRV,  KC_BSLS, KC_LCBR, KC_RCBR, _______,
-        _______, KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,
+        KC_DEL,  KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_DEL,
                                    _______, _______,   _______, _______
     ),
 
     [_NUMBERS] = LAYOUT_SUPER(
-        _______, KC_MSTP, KC_MPRV, KC_MNXT, _______,   KC_UNDS, KC_7,    KC_8,    KC_9,    _______,
-        _______, KC_VOLD, KC_VOLU, KC_MPLY, _______,   KC_SPC,  KC_4,    KC_5,    KC_6,    _______,
-        _______, KC_BRID, KC_BRIU, KC_MUTE, _______,   KC_0,    KC_1,    KC_2,    KC_3,    OS_NUMX,
+        _______, KC_MSTP, KC_MPRV, KC_MNXT, XXXXXXX,   KC_UNDS, KC_7,    KC_8,    KC_9,    _______,
+        _______, KC_VOLD, KC_VOLU, KC_MPLY, XXXXXXX,   KC_SPC,  KC_4,    KC_5,    KC_6,    _______,
+        XXXXXXX, KC_BRID, KC_BRIU, KC_MUTE, XXXXXXX,   KC_0,    KC_1,    KC_2,    KC_3,    OS_NUMX,
                                    _______, _______,   _______, _______
     ),
 
     [_NUMBERS_AUX] = LAYOUT_SUPER(
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,    _______, _______, _______, _______, _______,
         _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,    _______, KC_PLUS, KC_ASTR, KC_DOT,  _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,    KC_COLN, KC_MINS, KC_SLSH, KC_COMM, KC_NO,
+        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,    KC_COLN, KC_MINS, KC_SLSH, KC_COMM, KC_NO,
                                    _______, _______,   _______, _______
     ),
 
     [_ARROWS] = LAYOUT_SUPER(
-        _______, KC_WH_D, KC_MS_U, KC_WH_U, RESET,     _______, _______, _______, _______, _______,
-        _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, KC_BTN2, _______, _______, EP_HOME,   _______, _______, _______, _______, _______,
+        _______, KC_WH_D, KC_MS_U, KC_WH_U, RESET,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, EP_HOME,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                    _______, _______,   _______, _______
     ),
 
     [_EPHEMERAL_HOME] = LAYOUT(
-        TO(_SUDOKU), TO(_WASD), TO(_WASD_ARROWS), _______,        _______,
-        _______,     _______,   _______,          _______,        _______,
+        TO(_SUDOKU), TO(_WASD), TO(_WASD_ARROWS), XXXXXXX,        XXXXXXX,
+        XXXXXXX,     XXXXXXX,   XXXXXXX,          XXXXXXX,        XXXXXXX,
 
-        _______,     _______,   _______,          _______,        _______,
-        _______,     _______,   _______,          _______,        _______,
+        XXXXXXX,     XXXXXXX,   XXXXXXX,          XXXXXXX,        XXXXXXX,
+        XXXXXXX,     XXXXXXX,   XXXXXXX,          XXXXXXX,        XXXXXXX,
 
-        _______,     _______,   _______,          _______,        _______,
-        _______,     _______,   _______,          _______,        _______,
+        XXXXXXX,     XXXXXXX,   XXXXXXX,          XXXXXXX,        XXXXXXX,
+        XXXXXXX,     XXXXXXX,   XXXXXXX,          XXXXXXX,        XXXXXXX,
 
         _______,        TO(_QWERTY),
         TO(_SPECIAL),   _______
@@ -156,3 +168,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______,   TO(_QWERTY), _______
     )
 };
+// clang-format on
