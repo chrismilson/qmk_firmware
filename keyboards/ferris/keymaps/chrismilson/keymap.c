6ebb5707b849f7431d8c10dcad7a934af40762c0
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2024-11-04 21:45:15 -0500

    Use reboot

--- keyboards/ferris/keymaps/chrismilson/keymap.c
+++ keyboards/ferris/keymaps/chrismilson/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_ARROWS] = LAYOUT_SUPER(
-        _______, KC_WH_D, KC_MS_U, KC_WH_U, RESET,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_WH_D, KC_MS_U, KC_WH_U, QK_RBT,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
         _______, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
         KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, EP_HOME,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                    _______, _______,   _______, _______
